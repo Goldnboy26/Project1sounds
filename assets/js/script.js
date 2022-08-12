9ea0bbbfd4b0be4db9fc447e16ff6b641ae1dfd5
Noahb26 <noahbrown2663@gmail.com>
2022-08-12 12:31:06 -0700

    add sidebar to favorites page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
+
 let term = '';
-const updateTerm = () => {
-  //event.preventDefault()
+const updateTerm = (event) => {
+  event.preventDefault()
   term = document.getElementById('searchTerm').value;
 
   if (!term || term === '') {
    alert('Please enter a seach term');
+   
 } else {
-  const url = `https://itunes.apple.com/search?term=${term}` + `&entity=song&limit=75`;
+  let url = `https://itunes.apple.com/search?term=${term}` + `&entity=song&limit=75`;
   fetch(url)
   .then(function (response) {
   return response.json();
 })
 .then(function (data) {
+  
   console.log(data);
 });
 
@@ -19,12 +22,14 @@ const updateTerm = () => {
 
 };
 
+//document.location.replaceWith('./assets/searchresults.html');
+
+
 const searchBtn = document.getElementById('searchTermBtn');
   
   searchBtn.addEventListener('click', updateTerm);
+
+    
   
-//$('searchTermBtn').on('click', updateTerm);
-
-
 
 
