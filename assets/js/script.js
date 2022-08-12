bebd30b600bf1cfa54cba07c1c02ee41461f717f
Noahb26 <noahbrown2663@gmail.com>
2022-08-11 21:19:19 -0700

    add fetch function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+let term = '';
+const updateTerm = () => {
+  //event.preventDefault()
+  term = document.getElementById('searchTerm').value;
 
-const clearInput = () => {
-    const input = document.getElementsByTagName("input")[0];
-    input.value = "";
-  }
+  if (!term || term === '') {
+   alert('Please enter a seach term');
+} else {
+  const url = `https://itunes.apple.com/search?term=${term}` + `&entity=song&limit=75`;
+  fetch(url)
+  .then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
+
+}
+
+};
+
+const searchBtn = document.getElementById('searchTermBtn');
+  
+  searchBtn.addEventListener('click', updateTerm);
+  
+//$('searchTermBtn').on('click', updateTerm);
+
+
+
+
